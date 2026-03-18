d6cbffd0f34c9e475cfc9b239b7a822202a60a81
HoangNgocNhi <23020130@vnu.edu.vn>
2026-03-19 01:14:01 +0700

    Week4 V3 Demo Get a book

--- Buoi4/server.py
+++ Buoi4/server.py
@@ -26,10 +26,12 @@ def send_response(success=True, data=None, message="", status_code=200):
 
 @app.route('/v1/books', methods=['GET'])
 @app.route('/v2/books', methods=['GET'])
+@app.route('/v3/books', methods=['GET'])
 def get_books():
     return send_response(data=books, message="Successfully retrieved the list of books!", status_code=200)
 
 @app.route('/v2/books', methods=['POST'])
+@app.route('/v3/books', methods=['POST'])
 def add_book():
     data = request.json
     if not data or 'title' not in data or 'author' not in data or 'isbn' not in data:
@@ -43,6 +45,13 @@ def add_book():
     }
     books.append(new_book)
     return send_response(data=new_book, message="New book added successfully!", status_code=201)
+
+@app.route('/v3/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = next((b for b in books if b['id'] == id), None)
+    if not book:
+        return send_response(success=False, message="Not found!", status_code=404)
+    return send_response(data=book, message="Successfully retrieved a book!", status_code=200)
 
 if __name__ == '__main__':
     app.run(debug=True, port = 1604)
