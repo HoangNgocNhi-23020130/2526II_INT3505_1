dd911412415ee61b2e991533ae8c6e76f3b8c624
HoangNgocNhi <23020130@vnu.edu.vn>
2026-03-19 01:06:50 +0700

    Week4 V2 Demo post

--- Buoi4/server.py
+++ Buoi4/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify, request
 from flasgger import Swagger
 
 app = Flask(__name__)
@@ -25,8 +25,24 @@ def send_response(success=True, data=None, message="", status_code=200):
     }), status_code
 
 @app.route('/v1/books', methods=['GET'])
+@app.route('/v2/books', methods=['GET'])
 def get_books():
     return send_response(data=books, message="Successfully retrieved the list of books!", status_code=200)
+
+@app.route('/v2/books', methods=['POST'])
+def add_book():
+    data = request.json
+    if not data or 'title' not in data or 'author' not in data or 'isbn' not in data:
+        return send_response(success=False, message="Missing required data!", status_code=400)
+    
+    new_book = {
+        "id": books[-1]['id'] + 1 if books else 1,
+        "title": data['title'],
+        "author": data['author'],
+        "isbn": data['isbn']
+    }
+    books.append(new_book)
+    return send_response(data=new_book, message="New book added successfully!", status_code=201)
 
 if __name__ == '__main__':
     app.run(debug=True, port = 1604)
