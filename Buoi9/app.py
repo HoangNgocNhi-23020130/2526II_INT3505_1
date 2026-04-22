4f43801ffbc76f7d6f88bc23c820a5168cba2523
HoangNgocNhi <23020130@vnu.edu.vn>
2026-04-23 02:30:26 +0700

    Week9 version 2

--- Buoi9/app.py
+++ Buoi9/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from api.v1.payment_controller import api_v1
+from api.v2.payment_controller import api_v2
 
 app = Flask(__name__)
 
-# Đăng ký blueprint v1
+# Đăng ký blueprint
 app.register_blueprint(api_v1, url_prefix='/api/v1')
+app.register_blueprint(api_v2, url_prefix='/api/v2')
 
 if __name__ == '__main__':
+    print("Version 1: http://127.0.0.1:1604/api/v1/payments")
+    print("Version 2: http://127.0.0.1:1604/api/v2/payments")
     app.run(debug=True, port=1604)
